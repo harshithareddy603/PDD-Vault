eb24f67d3d13bcdb39b1d133345c638043c601e7
harshithareddy603 <harshitareddy919@gmail.com>
2026-08-19 16:45:40 +0530

    Add @expo/vector-icons type declarations in vite-env.d.ts

--- Frontend/src/vite-env.d.ts
+++ Frontend/src/vite-env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="vite/client" />
+
+declare module '@expo/vector-icons' {
+  import { ComponentType } from 'react';
+  export const MaterialCommunityIcons: ComponentType<any>;
+  export const Feather: ComponentType<any>;
+  export const AntDesign: ComponentType<any>;
+  export const Ionicons: ComponentType<any>;
+  export const FontAwesome: ComponentType<any>;
+  export const MaterialIcons: ComponentType<any>;
+  export const Entypo: ComponentType<any>;
+  export const EvilIcons: ComponentType<any>;
+  export const Octicons: ComponentType<any>;
+  export const SimpleLineIcons: ComponentType<any>;
+  export const Zocial: ComponentType<any>;
+  export const Foundation: ComponentType<any>;
+  const DefaultIcon: ComponentType<any>;
+  export default DefaultIcon;
+}
+
+declare module '@expo/vector-icons/*' {
+  import { ComponentType } from 'react';
+  const IconComponent: ComponentType<any>;
+  export default IconComponent;
+}
+
